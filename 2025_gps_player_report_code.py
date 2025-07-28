ca191e562505da8b2147013b1a040be1589249e2
Snorkcity <scott.conlon10@gmail.com>
2025-07-28 14:32:40 +1000

    formatiing

--- 2025_gps_player_report_code.py
+++ 2025_gps_player_report_code.py
@@ -480,13 +480,13 @@ def create_distance_per_min_chart(df_filtered, selected_player, sort_order):
         xaxis=dict(
             showline=True,
             showgrid=False,
-            tickfont=dict(size=14, family='Segoe UI', size=14, color='white')
+            tickfont=dict(family='Segoe UI', size=14, color='white')
         ),
         yaxis=dict(
             showline=True,
             gridcolor='gray',
             zeroline=True,
-            tickfont=dict(size=14, family='Segoe UI', size=14, color='white')
+            tickfont=dict(family='Segoe UI', size=14, color='white')
         ),
         margin=dict(l=20, r=20, t=60, b=20),
         legend=dict(
